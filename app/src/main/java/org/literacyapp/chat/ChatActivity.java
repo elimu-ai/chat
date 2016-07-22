4e0e0c20b2868b86843f5f5e5c6e4ecfd8b29bc5
Jo Grimstad <jo@literacyapp.org>
2016-07-22 09:54:12 +0300

    #LRUrY8TB Initial UI layout

--- app/src/main/java/org/literacyapp/chat/ChatActivity.java
+++ app/src/main/java/org/literacyapp/chat/ChatActivity.java
@@ -1,16 +1,14 @@
-package org.literacyapp.chat.main;
+package org.literacyapp.chat;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-import org.literacyapp.chat.R;
-
-public class MainActivity extends Activity {
+public class ChatActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
     }
 }
