f906d748091357b6079c0e72db8ca540dd3658c5
Jo Grimstad <jo@literacyapp.org>
2016-12-11 23:14:45 +0300

    #31 Create StudentUpdateReceiver

--- app/src/main/java/org/literacyapp/chat/receiver/StudentUpdateReceiver.java
+++ app/src/main/java/org/literacyapp/chat/receiver/StudentUpdateReceiver.java
@@ -3,15 +3,34 @@ package org.literacyapp.chat.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
 
 public class StudentUpdateReceiver extends BroadcastReceiver {
-    public StudentUpdateReceiver() {
-    }
+
+    public static final String PREF_STUDENT_ID = "pref_student_id";
+    public static final String PREF_STUDENT_AVATAR = "pref_student_avatar";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.i(getClass().getName(), "onReceive");
+
+        String studentId = intent.getStringExtra("studentId");
+        Log.i(getClass().getName(), "studentId: " + studentId);
+
+        String studentAvatar = intent.getStringExtra("studentAvatar");
+        Log.i(getClass().getName(), "studentAvatar: " + studentAvatar);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (!TextUtils.isEmpty(studentId)) {
+            sharedPreferences.edit().putString(PREF_STUDENT_ID, studentId).commit();
+        }
+
+        if (!TextUtils.isEmpty(studentAvatar)) {
+            sharedPreferences.edit().putString(PREF_STUDENT_AVATAR, studentAvatar).commit();
+        }
     }
 }
