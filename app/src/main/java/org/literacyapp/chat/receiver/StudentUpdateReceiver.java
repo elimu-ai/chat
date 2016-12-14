0520e62cb7a0f1b7fe9277eebf4a213f7a248f26
Jo Grimstad <jo@literacyapp.org>
2016-12-14 18:20:23 +0300

    Improved UI

--- app/src/main/java/org/literacyapp/chat/receiver/StudentUpdateReceiver.java
+++ app/src/main/java/org/literacyapp/chat/receiver/StudentUpdateReceiver.java
@@ -8,6 +8,13 @@ import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.literacyapp.chat.ChatApplication;
+import org.literacyapp.chat.dao.MessageDao;
+import org.literacyapp.chat.model.Message;
+import org.literacyapp.chat.util.DeviceInfoHelper;
+
+import java.util.List;
+
 public class StudentUpdateReceiver extends BroadcastReceiver {
 
     public static final String PREF_STUDENT_ID = "pref_student_id";
@@ -26,6 +33,26 @@ public class StudentUpdateReceiver extends BroadcastReceiver {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (!TextUtils.isEmpty(studentId)) {
+            String existingStudentId = sharedPreferences.getString(PREF_STUDENT_ID, null);
+            Log.i(getClass().getName(), "existingStudentId: " + existingStudentId);
+            if (TextUtils.isEmpty(existingStudentId)) {
+                // Update previously sent messages on the current device
+                ChatApplication chatApplication = (ChatApplication) context.getApplicationContext();
+                MessageDao messageDao = chatApplication.getDaoSession().getMessageDao();
+                List<Message> existingMessages = messageDao.queryBuilder()
+                        .where(
+                                MessageDao.Properties.DeviceId.eq(DeviceInfoHelper.getDeviceId(context)),
+                                MessageDao.Properties.StudentId.isNull()
+                        )
+                        .list();
+                Log.i(getClass().getName(), "existingMessages.size(): " + existingMessages.size());
+                for (Message message : existingMessages) {
+                    message.setStudentId(studentId);
+                    message.setStudentAvatar(studentAvatar);
+                    messageDao.update(message);
+                }
+            }
+
             sharedPreferences.edit().putString(PREF_STUDENT_ID, studentId).commit();
         }
 
