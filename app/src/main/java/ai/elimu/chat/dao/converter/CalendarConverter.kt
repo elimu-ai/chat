4035e2e7363cb883dcccf49e2c4d568af0ee7eaf
tuancoltech <tuancoltech@gmail.com>
2025-05-10 17:00:59 +0700

    Convert CalendarConverter to Kotlin

--- app/src/main/java/ai/elimu/chat/dao/converter/CalendarConverter.kt
+++ app/src/main/java/ai/elimu/chat/dao/converter/CalendarConverter.kt
@@ -1,21 +1,20 @@
-package ai.elimu.chat.dao.converter;
+package ai.elimu.chat.dao.converter
 
-import org.greenrobot.greendao.converter.PropertyConverter;
+import org.greenrobot.greendao.converter.PropertyConverter
+import java.util.Calendar
 
-import java.util.Calendar;
+class CalendarConverter : PropertyConverter<Calendar?, Long?> {
 
-public class CalendarConverter implements PropertyConverter<Calendar, Long> {
-
-    @Override
-    public Calendar convertToEntityProperty(Long databaseValue) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(databaseValue);
-        return calendar;
+    override fun convertToEntityProperty(databaseValue: Long?): Calendar? {
+        val calendar = Calendar.getInstance()
+        databaseValue?.let {
+            calendar.setTimeInMillis(databaseValue)
+        }
+        return calendar
     }
 
-    @Override
-    public Long convertToDatabaseValue(Calendar entityProperty) {
-        Long databaseValue = entityProperty.getTimeInMillis();
-        return databaseValue;
+    override fun convertToDatabaseValue(entityProperty: Calendar?): Long? {
+        val databaseValue = entityProperty?.getTimeInMillis()
+        return databaseValue
     }
 }
