188898dfaa22ce964916f30d14d722b0c75672de
tuancoltech <tuancoltech@gmail.com>
2025-05-10 16:57:54 +0700

    Convert StudentUpdateReceiver to Kotlin

--- app/src/main/java/ai/elimu/chat/receiver/StudentUpdateReceiver.kt
+++ app/src/main/java/ai/elimu/chat/receiver/StudentUpdateReceiver.kt
@@ -1,63 +1,58 @@
-package ai.elimu.chat.receiver;
+package ai.elimu.chat.receiver
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.text.TextUtils;
-import android.util.Log;
+import ai.elimu.chat.ChatApplication
+import ai.elimu.chat.dao.MessageDao
+import ai.elimu.chat.util.DeviceInfoHelper.getDeviceId
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.preference.PreferenceManager
+import android.text.TextUtils
+import android.util.Log
 
-import ai.elimu.chat.ChatApplication;
-import ai.elimu.chat.dao.MessageDao;
-import ai.elimu.chat.model.Message;
-import ai.elimu.chat.util.DeviceInfoHelper;
+class StudentUpdateReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(javaClass.getName(), "onReceive")
 
-import java.util.List;
+        val studentId = intent.getStringExtra("studentId")
+        Log.i(javaClass.getName(), "studentId: " + studentId)
 
-public class StudentUpdateReceiver extends BroadcastReceiver {
+        val studentAvatar = intent.getStringExtra("studentAvatar")
+        Log.i(javaClass.getName(), "studentAvatar: " + studentAvatar)
 
-    public static final String PREF_STUDENT_ID = "pref_student_id";
-    public static final String PREF_STUDENT_AVATAR = "pref_student_avatar";
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Log.i(getClass().getName(), "onReceive");
-
-        String studentId = intent.getStringExtra("studentId");
-        Log.i(getClass().getName(), "studentId: " + studentId);
-
-        String studentAvatar = intent.getStringExtra("studentAvatar");
-        Log.i(getClass().getName(), "studentAvatar: " + studentAvatar);
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
         if (!TextUtils.isEmpty(studentId)) {
-            String existingStudentId = sharedPreferences.getString(PREF_STUDENT_ID, null);
-            Log.i(getClass().getName(), "existingStudentId: " + existingStudentId);
+            val existingStudentId = sharedPreferences.getString(PREF_STUDENT_ID, null)
+            Log.i(javaClass.getName(), "existingStudentId: " + existingStudentId)
             if (TextUtils.isEmpty(existingStudentId)) {
                 // Update previously sent messages on the current device
-                ChatApplication chatApplication = (ChatApplication) context.getApplicationContext();
-                MessageDao messageDao = chatApplication.getDaoSession().getMessageDao();
-                List<Message> existingMessages = messageDao.queryBuilder()
-                        .where(
-                                MessageDao.Properties.DeviceId.eq(DeviceInfoHelper.getDeviceId(context)),
-                                MessageDao.Properties.StudentId.isNull()
-                        )
-                        .list();
-                Log.i(getClass().getName(), "existingMessages.size(): " + existingMessages.size());
-                for (Message message : existingMessages) {
-                    message.setStudentId(studentId);
-                    message.setStudentAvatar(studentAvatar);
-                    messageDao.update(message);
+                val chatApplication = context.getApplicationContext() as ChatApplication
+                val messageDao = chatApplication.daoSession!!.getMessageDao()
+                val existingMessages = messageDao.queryBuilder()
+                    .where(
+                        MessageDao.Properties.DeviceId.eq(getDeviceId(context)),
+                        MessageDao.Properties.StudentId.isNull()
+                    )
+                    .list()
+                Log.i(javaClass.getName(), "existingMessages.size(): " + existingMessages.size)
+                for (message in existingMessages) {
+                    message.setStudentId(studentId)
+                    message.setStudentAvatar(studentAvatar)
+                    messageDao.update(message)
                 }
             }
 
-            sharedPreferences.edit().putString(PREF_STUDENT_ID, studentId).commit();
+            sharedPreferences.edit().putString(PREF_STUDENT_ID, studentId).commit()
         }
 
         if (!TextUtils.isEmpty(studentAvatar)) {
-            sharedPreferences.edit().putString(PREF_STUDENT_AVATAR, studentAvatar).commit();
+            sharedPreferences.edit().putString(PREF_STUDENT_AVATAR, studentAvatar).commit()
         }
+    }
+
+    companion object {
+        const val PREF_STUDENT_ID: String = "pref_student_id"
+        const val PREF_STUDENT_AVATAR: String = "pref_student_avatar"
     }
 }
